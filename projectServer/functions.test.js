15fe6ba9d50102ce57ba11e5164ee8479044851e
Nathan Krasney Laptop <natankrasney@gmail.com>
2019-09-24 14:16:19 +0300

    jest playground

---
+++ projectServer/functions.test.js
@@ -0,0 +1,9 @@
+const math = require("./functions");
+
+test("Adding 1 + 1 equals 2", () => {
+    expect(math.sum(1, 1)).toBe(2);
+   });
+   
+//    test("Adding 1 + 1 not equals 3", () => {
+//     expect(math.sum(1, 1)).toBe(2);
+//    });
