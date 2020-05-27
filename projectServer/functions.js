2967d1dd80d67a9faae589daf0a3c5cf1f7402a8
Nathan Krasney <natankrasney@gmail.com>
2020-05-27 14:06:38 +0300

    add mock

--- projectServer/functions.js
+++ projectServer/functions.js
@@ -1,5 +1,9 @@
-function sum(num1,num2){
+function sum2(num1,num2){
     return num1+num2;
 }
 
-module.exports.sum = sum;
+function sum3(num1,num2,num3){
+    return sum2(num1,num2)+num3;
+}
+
+module.exports = {sum2,sum3};
--- projectServer/functions.test.js
+++ projectServer/functions.test.js
@@ -1,16 +1,31 @@
 const math = require("./functions");
 const constants = require("./constants");
 
-constants.a = 2;
-
-test("change constant a to 2 is success", () => {
-  expect(constants.a).toBe(2);
+// --- simple tests
+test("change constant a to 3 is success", () => {
+  constants.a = 3;
+  expect(constants.a).toBe(3);
 });
 
 test("Adding 1 + 1 equals 2", () => {
-  expect(math.sum(1, 1)).toBe(2);
+  expect(math.sum2(1, 1)).toBe(2);
 });
 
 test("Adding 1 + 1 not equals 3", () => {
-  expect(math.sum(1, 1)).not.toBe(3);
+  expect(math.sum2(1, 1)).not.toBe(3);
 });
+
+test("Adding 1 + 2 + 3 not equals 7", () => {
+  expect(math.sum3(1, 2, 3)).not.toBe(7);
+});
+
+test("Adding 1 + 2 + 3  equals 6", () => {
+  expect(math.sum3(1, 2, 3)).toBe(6);
+});
+
+test("sum2 is called once", () => {
+  math.sum2 = jest.fn((a, b) => {});
+  math.sum2(1, 2);
+  expect(math.sum2).toHaveBeenCalled();
+});
+
