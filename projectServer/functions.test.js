e2ef367b69fcf590e8af9ffa25cd7470f0139f45
Nathan Krasney <natankrasney@gmail.com>
2020-05-27 14:13:53 +0300

    ...

--- projectServer/functions.test.js
+++ projectServer/functions.test.js
@@ -7,6 +7,7 @@ test("change constant a to 3 is success", () => {
   expect(constants.a).toBe(3);
 });
 
+
 test("Adding 1 + 1 equals 2", () => {
   expect(math.sum2(1, 1)).toBe(2);
 });
@@ -23,6 +24,8 @@ test("Adding 1 + 2 + 3  equals 6", () => {
   expect(math.sum3(1, 2, 3)).toBe(6);
 });
 
+
+// --- using mocks
 test("sum2 is called once", () => {
   math.sum2 = jest.fn((a, b) => {});
   math.sum2(1, 2);
