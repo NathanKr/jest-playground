da7ff31bd126fd3233cfa2c222853acf51cfc8f3
Nathan Krasney <natankrasney@gmail.com>
2020-05-27 12:49:27 +0300

    mock constant

--- projectServer/functions.test.js
+++ projectServer/functions.test.js
@@ -1,9 +1,16 @@
 const math = require("./functions");
+const constants = require("./constants");
+
+constants.a = 2;
+
+test("change constant a to 2 is success", () => {
+  expect(constants.a).toBe(2);
+});
 
 test("Adding 1 + 1 equals 2", () => {
-    expect(math.sum(1, 1)).toBe(2);
-   });
-   
-//    test("Adding 1 + 1 not equals 3", () => {
-//     expect(math.sum(1, 1)).toBe(2);
-//    });
+  expect(math.sum(1, 1)).toBe(2);
+});
+
+test("Adding 1 + 1 not equals 3", () => {
+  expect(math.sum(1, 1)).not.toBe(3);
+});
